7377d2d0ed5d6e3a6b5ac24249b73f72db726749
TAEYANG YOO <yty0706@gmail.com>
2022-01-14 22:48:08 +0900

    레이아웃 수정

--- js/background.js
+++ js/background.js
@@ -5,12 +5,17 @@ const images = [
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const imgSite = document.querySelector(".screen-main .screen-main__img");
+// const imgSite = document.querySelector(".screen-main .screen-main__img");
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-bgImage.style.width = "100%";
-bgImage.style.height = "100%";
+const imgSite = document.querySelector(".screen-main");
+imgSite.style.backgroundImage = "url("+`img/${chosenImage}`+")";
+imgSite.style.backgroundSize = "50% 50%";
+// const bgImage = document.createElement("img");
+// bgImage.src = `img/${chosenImage}`;
+// bgImage.style.width = "100%";
+// bgImage.style.height = "100%";
 
 
-imgSite.appendChild(bgImage);
+// imgSite.appendChild(bgImage);
+
+console.dir(imgSite);
