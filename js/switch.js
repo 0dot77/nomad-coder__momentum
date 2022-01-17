5f9868de6e222a16a2307d06756f35fc680c8696
TAEYANG YOO <yty0706@gmail.com>
2022-01-17 22:54:30 +0900

    feat : 개편
    - 백그라운드 없앰
    - 레이아웃 수정
    - js 작업중

---
+++ js/switch.js
@@ -0,0 +1,9 @@
+const rightArrowBtn = document.getElementById("right-arrow");
+const screenComponents = document.getElementById("section-first");
+
+
+// console.log(screenComponents);
+
+rightArrowBtn.addEventListener("click", function(){
+
+});
