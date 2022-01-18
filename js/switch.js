e0177d12dfc71286cca66b52c585dbfd4bbdaf38
TAEYANG YOO <yty0706@gmail.com>
2022-01-18 13:28:17 +0900

    feat : 레이아웃 수정 및 switch js 추가

--- js/switch.js
+++ js/switch.js
@@ -1,9 +1,30 @@
 const rightArrowBtn = document.getElementById("right-arrow");
 const screenComponents = document.getElementById("section-first");
-
+const todoSection = document.getElementById("section-second");
+const leftArrowBtn = document.getElementById("left-arrow");
 
 // console.log(screenComponents);
+todoSection.style.left = "1200px";
+screenComponents.style.right = "0px";
+screenComponents.style.left = "200px";
 
-rightArrowBtn.addEventListener("click", function(){
+function handdleScreenComponents() {
+    screenComponents.style.right = "1500px";
+    screenComponents.style.left = "-1000px";
+    todoSection.style.left = "-300px";
+}
 
-});
+function handdleTodoList() {
+    todoSection.style.left = "1200px";
+    screenComponents.style.right = "0px";
+    screenComponents.style.left = "200px";
+}
+
+rightArrowBtn.addEventListener("click", handdleScreenComponents);
+leftArrowBtn.addEventListener("click", handdleTodoList);
+
+// if (!switching) {
+//     todoSection.style.left = "1000px";
+// } else {
+//     console.log(switching);
+// }
